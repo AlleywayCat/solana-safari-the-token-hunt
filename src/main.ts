d2521e24e44ea3606bf204ca031aa86bc3b33e90
Nikola Lausev <nikola.lausev@vegait.rs>
2024-07-19 13:43:49 +0200

    Implement fetching balance and accounts from the provided public key. Integration with the Solana RPC

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './shared/logging/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
+  app.useGlobalInterceptors(new LoggingInterceptor());
 
   await app.listen(3000);
 }
